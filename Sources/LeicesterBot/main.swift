397cb9597ac36f8b4542da92d147987bd33ec382
Kirill Afonin <aku536@mail.ru>
2020-11-20 18:28:54 +0300

    Неанонимный опрос

--- Sources/LeicesterBot/main.swift
+++ Sources/LeicesterBot/main.swift
@@ -3,11 +3,18 @@ import TelegramBotSDK
 
 let token = readToken(from: "LEICESTER_BOT_TOKEN")
 let bot = TelegramBot(token: token)
+bot.autoReconnect = true
 
 while let update = bot.nextUpdateSync() {
     if let message = update.message, let from = message.from, let text = message.text {
         bot.sendMessageAsync(chatId: .chat(from.id),
                              text: "Hi \(from.firstName)! You said: \(text).\n")
+        if update.message?.text == "Опрос" {
+            bot.sendPollAsync(chatId: .chat(from.id),
+                              question: "Буду на игре:",
+                              options: ["Да", "Нет"],
+                              isAnonymous: false)
+        }
     }
 }
 
